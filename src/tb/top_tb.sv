32b1daaadd49886f8a976090cfe636f8bcc17012
codeQuanto <igorking314@gmail.com>
2025-12-15 00:18:33 +0100

    Update messy code which works

--- src/tb/top_tb.sv
+++ src/tb/top_tb.sv
@@ -1,4 +1,5 @@
 `include "vivado_interfaces.svh"
+import axi_vip_pkg::*;
 import axi_vip_0_pkg::*;
 
 module top_tb(
@@ -7,10 +8,11 @@ module top_tb(
   logic aclk;
   logic aresetn;
   logic [31:0] data;
-  
+
   axi_vip_0_mst_t axi_master;
 
   vivado_axi4_lite_v1_0 M_AXI();
+  axi4lite_if axi_if(aclk, aresetn);
   
   axi_vip_0_sv axi_vip_i (
     .M_AXI(M_AXI.master), // vivado_axi4_lite_v1_0.master M_AXI
@@ -18,26 +20,49 @@ module top_tb(
     .aresetn(aresetn)     // input wire aresetn
   );
 
+
+
   axi4lite_slave dut (
-    .A_CLK    (aclk),
-    .A_RSTn   (aresetn),
-    .AW_VALID (M_AXI.awvalid),
-    .AW_READY (M_AXI.awready),
-    .AW_ADDR  (M_AXI.awaddr),
-    .W_VALID  (M_AXI.wvalid),
-    .W_READY  (M_AXI.wready),
-    .W_DATA   (M_AXI.wdata),
-    .B_VALID  (M_AXI.bvalid),
-    .B_READY  (M_AXI.bready),
-    .B_RESP   (M_AXI.bresp),
-    .AR_VALID (M_AXI.arvalid),
-    .AR_READY (M_AXI.arready),
-    .AR_ADDR  (M_AXI.araddr),
-    .R_VALID  (M_AXI.rvalid),
-    .R_READY  (M_AXI.rready),
-    .R_DATA   (M_AXI.rdata),
-    .R_RESP   (M_AXI.rresp)
+    //.A_CLK    (aclk),
+    //.A_RSTn   (aresetn),
+    .axi_if   (axi_if.slave)
+
+    /*
+    .AW_VALID (M_AXI.m_axi_awvalid),
+    .AW_READY (M_AXI.m_axi_awready),
+    .AW_ADDR  (M_AXI.m_axi_awaddr),
+    .W_VALID  (M_AXI.m_axi_wvalid),
+    .W_READY  (M_AXI.m_axi_wready),
+    .W_DATA   (M_AXI.m_axi_wdata),
+    .B_VALID  (M_AXI.m_axi_bvalid),
+    .B_READY  (M_AXI.m_axi_bready),
+    .B_RESP   (M_AXI.m_axi_bresp),
+    .AR_VALID (M_AXI.m_axi_arvalid),
+    .AR_READY (M_AXI.m_axi_arready),
+    .AR_ADDR  (M_AXI.m_axi_araddr),
+    .R_VALID  (M_AXI.m_axi_rvalid),
+    .R_READY  (M_AXI.m_axi_rready),
+    .R_DATA   (M_AXI.m_axi_rdata),
+    .R_RESP   (M_AXI.m_axi_rresp) */
   );
+
+assign axi_if.AW_VALID = M_AXI.AWVALID;
+assign axi_if.AW_ADDR  = M_AXI.AWADDR;
+assign axi_if.W_VALID  = M_AXI.WVALID;
+assign axi_if.W_DATA   = M_AXI.WDATA;
+assign axi_if.B_READY  = M_AXI.BREADY;
+assign axi_if.AR_VALID = M_AXI.ARVALID;
+assign axi_if.AR_ADDR  = M_AXI.ARADDR;
+assign axi_if.R_READY  = M_AXI.RREADY;
+
+assign M_AXI.AWREADY = axi_if.AW_READY;
+assign M_AXI.WREADY  = axi_if.W_READY;
+assign M_AXI.BVALID  = axi_if.B_VALID;
+assign M_AXI.BRESP   = axi_if.B_RESP;
+assign M_AXI.ARREADY = axi_if.AR_READY;
+assign M_AXI.RVALID  = axi_if.R_VALID;
+assign M_AXI.RDATA   = axi_if.R_DATA;
+assign M_AXI.RRESP   = axi_if.R_RESP;
 
 initial aclk = 0;
 always #10 aclk = ~aclk;
@@ -53,11 +78,12 @@ initial begin
   wait(aresetn == 1);
   @(posedge aclk);
 
-  axi_master = new("axi_master", axi_vip_i.inst.IF);
+  axi_master = new("axi_master", top_tb.axi_vip_i.inst.inst.IF);
   axi_master.start_master();
 
-  axi_master.write(32'h0000_0004, 32'h1234_5678);
-  axi_master.read (32'h0000_0004, data);
+  // tutaj trzeba poczytac w poradniku VIPa jak to zrobic. Widzialem inne komendy, inny 'workflow'
+  //axi_master.write(32'h0000_0004, 32'h1234_5678);
+  //axi_master.read (32'h0000_0004, data);
 end
 
 endmodule
--- src/v/axi4lite_slave.sv
+++ src/v/axi4lite_slave.sv
@@ -1,8 +1,10 @@
-
+//`include "params.vh"
 module axi4lite_slave (
   // Global
-  input  logic A_CLK, 
-  input  logic A_RSTn,
+  // /input  logic A_CLK, 
+  // /input  logic A_RSTn,
+  axi4lite_if  axi_if
+  /*
   // AW channel
   input  logic                      AW_VALID,
   output logic                      AW_READY,
@@ -26,7 +28,7 @@ module axi4lite_slave (
   output logic                      R_VALID,
   input  logic                      R_READY,
   output logic [AXI_DATA_WIDTH-1:0] R_DATA,
-  output logic [1:0]                R_RESP
+  output logic [1:0]                R_RESP */
 );
 
   // declaring internal signals
@@ -44,32 +46,32 @@ module axi4lite_slave (
 
   // reg_bank instantion
   reg_bank reg_bank_i(
-    .clk         (A_CLK),
-    .reset_n     (A_RSTn),
+    .clk         (axi_if.A_CLK),
+    .reset_n     (axi_if.A_RSTn),
     .write_en    (write_en),
-    .write_addr  (AW_ADDR),
-    .write_data  (W_DATA),
-    .read_addr   (AR_ADDR),
+    .write_addr  (axi_if.AW_ADDR),
+    .write_data  (axi_if.W_DATA),
+    .read_addr   (axi_if.AR_ADDR),
     .read_data   (read_data)
   );
 
   // Setting proper flags based on current state
   // AW
-  assign AW_READY    = (curr_state == WRITE)     ? 1'b1 : 1'b0;
+  assign axi_if.AW_READY    = (curr_state == WRITE)     ? 1'b1 : 1'b0;
   // W
-  assign W_READY     = (curr_state == WRITE)     ? 1'b1 : 1'b0;
-  assign aw_handshake       = AW_VALID && AW_READY;
-  assign w_handshake        = W_VALID && W_READY;
+  assign axi_if.W_READY     = (curr_state == WRITE)     ? 1'b1 : 1'b0;
+  assign aw_handshake       = axi_if.AW_VALID && axi_if.AW_READY;
+  assign w_handshake        = axi_if.W_VALID && axi_if.W_READY;
   assign write_en           = aw_handshake_flag && w_handshake_flag;
   // B
-  assign B_VALID     = (curr_state == W_RESP)    ? 1'b1 : 1'b0;
-  assign B_RESP      = (curr_state == W_RESP)    ? 2'b1 : 2'b0; // TODO response is always 1'b1 - just to observe the reaction (should be 0)
+  assign axi_if.B_VALID     = (curr_state == W_RESP)    ? 1'b1 : 1'b0;
+  assign axi_if.B_RESP      = (curr_state == W_RESP)    ? 2'b1 : 2'b0; // TODO response is always 1'b1 - just to observe the reaction (should be 0)
   // AR
-  assign AR_READY    = (curr_state == READ_ADDR) ? 1'b1 : 1'b0;
+  assign axi_if.AR_READY    = (curr_state == READ_ADDR) ? 1'b1 : 1'b0;
   // R
-  assign R_VALID     = (curr_state == READ_DATA) ? 1'b1 : 1'b0;
-  assign R_DATA      = (curr_state == READ_DATA) ? read_data : {AXI_DATA_WIDTH{1'b0}};
-  assign R_RESP      = (curr_state == READ_DATA) ? 2'b1 : 2'b0; // TODO response is always 1'b1 - just to observe the reaction (should be 0)
+  assign axi_if.R_VALID     = (curr_state == READ_DATA) ? 1'b1 : 1'b0;
+  assign axi_if.R_DATA      = (curr_state == READ_DATA) ? read_data : {AXI_DATA_WIDTH{1'b0}};
+  assign axi_if.R_RESP      = (curr_state == READ_DATA) ? 2'b1 : 2'b0; // TODO response is always 1'b1 - just to observe the reaction (should be 0)
 
   always_comb begin : flag_handling_b
     case (curr_state)
@@ -84,8 +86,8 @@ module axi4lite_slave (
     endcase
   end : flag_handling_b
 
-  always_ff @(posedge A_CLK) begin : driving_state_b
-    if (!A_RSTn) begin
+  always_ff @(posedge axi_if.A_CLK) begin : driving_state_b
+    if (!axi_if.A_RSTn) begin
       curr_state <= IDLE;
     end else begin
       curr_state <= next_state;
@@ -95,18 +97,18 @@ module axi4lite_slave (
   always_comb begin : state_machine_logic_b
     case (curr_state)
       IDLE      : begin
-                    if (AR_VALID) begin
+                    if (axi_if.AR_VALID) begin
                       next_state = READ_ADDR;
-                    end else if (AW_VALID || W_VALID) begin
+                    end else if (axi_if.AW_VALID || axi_if.W_VALID) begin
                       next_state = WRITE;
                     end else begin
                       next_state = IDLE;
                     end
                   end
       WRITE     : if (aw_handshake_flag && w_handshake_flag) next_state = W_RESP;
-      W_RESP    : if (B_VALID  && B_READY)     next_state = IDLE;
-      READ_ADDR : if (AR_VALID && AR_READY)    next_state = READ_DATA;
-      READ_DATA : if (R_VALID  && R_READY)     next_state = IDLE;
+      W_RESP    : if (axi_if.B_VALID  && axi_if.B_READY)     next_state = IDLE;
+      READ_ADDR : if (axi_if.AR_VALID && axi_if.AR_READY)    next_state = READ_DATA;
+      READ_DATA : if (axi_if.R_VALID  && axi_if.R_READY)     next_state = IDLE;
       default   : next_state = IDLE;
     endcase
   end : state_machine_logic_b
